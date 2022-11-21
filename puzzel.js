a90c93f4d3e875343dc92f9e07a07eda1d0e89be
Richard Sprenger <richardharald@t-online.de>
2022-11-21 20:24:38 +0100

    added rotate feature to number

--- puzzel.js
+++ puzzel.js
@@ -1,5 +1,5 @@
 
-/* Subquare
+/* Square
 * Bitwise:
 * 1 = bottom triangle filled
 * 2 = left triangle filled
@@ -28,11 +28,23 @@ class _Number {
     */
     // Initialized with an 8
     squares = [[9, 15, 15, 3], [15, 0, 0 , 15], [15, 0, 0, 15], [13, 15, 15, 7], [15, 0, 0, 15], [15, 0, 0, 15], [12, 15, 15, 6]]
+
+    // 0 == upright
+    // 1 == turned 90°
+    // 2 == turned 180°
+    // 3 == turned 270°
+    orientation = 0
+    insertedX = -1
+    insertedY = -1
+    insertedOrientation = -1
+
     constructor(value) {
+        this.value = value
+        // Orientation: 0 = Up, 1 = 90° turned, 2 = 180° turned, 3 = 270° turned
         this.createNumber(value)
     }
 
-
+    // Assign corresponding triangle to the corresponding position in a 4 by 7 grid
     createNumber(value) {
         switch (value) {
             case 0: this.squares = [[9, 15, 15, 3], [15, 0, 0, 15], [15, 0, 0, 15], [1, 0, 0, 13], [15, 0, 0, 15], [15, 0, 0, 15], [12, 15, 15, 6]]; break;
@@ -47,78 +59,220 @@ class _Number {
             case 9: this.squares = [[9, 15, 15, 3], [15, 0, 0, 15], [15, 0, 0, 15], [12, 15, 15, 7], [0, 0, 0, 15], [0, 0, 0, 15], [8, 15, 15, 6]]; break;
         }
     }
-}
 
-function displayNumber(squares) {
-    console.log(squares)
-    var header = document.getElementById("header")
-    var main = document.createElement("div")
-    main.className = "number"
-    header.appendChild(main)
-    for (let i = 0; i < 7; i ++) {
-        for (let j = 0; j < 4; j++) {
-            let square = document.createElement("div");
-            square.className = "container"
-            main.appendChild(square)
-            /* Square
-            * Bitwise:
-            * 1 = bottom triangle filled
-            * 2 = left triangle filled
-            * 4 = top triangle filled
-            * 8 = right triangle filled
-            */
-            let triangle1 = document.createElement("div")
-            triangle1.className = "triangle triangle-top"
-            if (!((squares[i][j] & 4) === 4)) triangle1.className += " hidden"
-            let triangle2 = document.createElement("div")
-            triangle2.className = "triangle triangle-left"
-            if (!((squares[i][j] & 2) === 2)) triangle2.className += " hidden"
-            let triangle3 = document.createElement("div")
-            triangle3.className = "triangle triangle-right"
-            if (!((squares[i][j] & 8) === 8)) triangle3.className += " hidden"
-            let triangle4 = document.createElement("div")
-            triangle4.className = "triangle triangle-bottom"
-            if (!((squares[i][j] & 1) === 1)) triangle4.className += " hidden"
-            square.appendChild(triangle1)
-            square.appendChild(triangle2)
-            square.appendChild(triangle3)
-            square.appendChild(triangle4)
+    displayNumber() {
+        var header = document.getElementById("header")
+        var number = document.getElementById("header-number-" + this.value)
+        
+        if (number != null) header.removeChild(number)
+        number = document.createElement("div")
+        
+        number.className = "number"
+        number.style.gridTemplateColumns = "repeat(" + this.squares[0].length + ", 20px)"
+        number.style.gridTemplateRows = "repeat(" + this.squares.length + ", 20px)"
+        number.id = "header-number-" + this.value
+
+        // Add event listener for menue on number
+        var self = this;
+        number.addEventListener("click", function() {showMenu.call(self)}, false);
+        
+        var nodeAfter = document.getElementById("header-number-" + (this.value + 1))
+        if (nodeAfter != null) header.insertBefore(number, nodeAfter)
+        else header.appendChild(number)
+
+        this.insertSquares(number)
+    }
+
+    insertSquares(number) {
+        for (let i = 0; i < this.squares.length; i ++) {
+            for (let j = 0; j < this.squares[i].length; j++) {
+                let square = document.createElement("div");
+                square.className = "container"
+            
+                if (this.squares[i][j] == 15) {
+                    square.className += " full-background"
+                } else {
+                    let triangle1 = document.createElement("div")
+                    triangle1.className = "triangle triangle-top"
+                    if (!((this.squares[i][j] & 4) === 4)) triangle1.className += " hidden"
+                    square.appendChild(triangle1)
+                    
+                    let triangle2 = document.createElement("div")
+                    triangle2.className = "triangle triangle-left"
+                    if (!((this.squares[i][j] & 2) === 2)) triangle2.className += " hidden"
+                    square.appendChild(triangle2)
+                    
+                    let triangle3 = document.createElement("div")
+                    triangle3.className = "triangle triangle-right"
+                    if (!((this.squares[i][j] & 8) === 8)) triangle3.className += " hidden"
+                    square.appendChild(triangle3)
+                    
+                    let triangle4 = document.createElement("div")
+                    triangle4.className = "triangle triangle-bottom"
+                    if (!((this.squares[i][j] & 1) === 1)) triangle4.className += " hidden"
+                    square.appendChild(triangle4)
+                }
+                number.appendChild(square)       
+            }
         }
     }
+
+    // Rotates a number 90 degrees clock or anti clockwise
+    // if rotate90DegRight == true => rotate clockwise
+    // if rotate90DegRight == false => rotate clockwise
+    rotate(rotate90DegRight) {
+        // https://stackoverflow.com/questions/42519/how-do-you-rotate-a-two-dimensional-array
+        if (!rotate90DegRight) {
+            this.reverseRow() 
+            this.orientation--;
+            if (this.orientation < 0) this.orientation = 3;
+        }
+        this.transpose()
+        if (rotate90DegRight) {
+            this.reverseRow() 
+            this.orientation++;
+            if (this.orientation > 3) this.orientation = 0;
+        }
+        this.rotateTriangle(rotate90DegRight)
+        this.displayNumber()
+        showMenu.call(this)
+    }
+
+    reverseRow() {
+        for (let i = 0; i < this.squares.length; i ++) {
+            this.squares[i].reverse()
+        }
+    }
+
+    rotateTriangle(clockwise) {
+        for (let i = 0; i < this.squares.length; i ++) {
+            for (let j = 0; j < this.squares[i].length; j++) {
+                // 0 and 15 don't need to be rotated
+                if (!(this.squares[i][j] == 0 && this.squares[i][j] == 15)) {
+                    let sum = 0;
+                    // bottom -> left || bottom -> right
+                    if ((this.squares[i][j] & 1) === 1) sum += clockwise === true ? 2 : 8
+                    // left -> top || left -> bottom
+                    if ((this.squares[i][j] & 2) === 2) sum += clockwise === true ? 4 : 1
+                    // top -> right || top -> left
+                    if ((this.squares[i][j] & 4) === 4) sum += clockwise === true ? 8 : 2
+                    // right -> bottom || right -> top
+                    if ((this.squares[i][j] & 8) === 8) sum += clockwise === true ? 1 : 4
+                    this.squares[i][j] = sum
+                }
+            }
+        }
+    }
+
+    transpose() {
+        // https://stackoverflow.com/questions/17428587/transposing-a-2d-array-in-javascript
+        this.squares = this.squares[0].map((col, i) => this.squares.map(row => row[i]));
+    }
+
 }
+
+class Board {
+
+    board = new Array(13).fill(new Array(16))
+
+    constructor() {
+        this.createBoard()
+    }
+
+    createBoard() {
+        let grid = document.getElementById("grid")
+        if (grid) {
+            document.body.removeChild(grid);
+        }
+        grid = document.createElement("div");
+        grid.id = "grid"
+    
+        for (let i = 0; i < 13; i++) {
+            for (let j = 0; j < 16; j++) {
+                let square = document.createElement("div");
+                square.id = i + "-" + j
+                square.className = "container"
+                grid.appendChild(square)
+            }
+        }
+        document.body.appendChild(grid)
+    }
+
+}
+
 var number = []
 
 for (let i = 0; i < 10; i++) {
     number.push(new _Number(i))
-    displayNumber(number[i].squares)
+    number[i].displayNumber()
 }
 
-// Width: 16 squares; Height: 13 squares
-var bord = new Array(13).fill(new Array(16))
 
-function createBoard() {
-    let grid = document.getElementById("grid")
-    if (grid) {
-        document.body.removeChild(grid);
-    }
-    grid = document.createElement("div");
+function showMenu() {
 
-    for (let i = 0; i < 13; i++) {
-        for (let j = 0; j < 16; j++) {
-            
+    var value = this.value
+
+    var menu = document.getElementById("menu")
+    var container = document.getElementById("menu-container")
+    if (container != null) menu.removeChild(container)
+    container = document.createElement("div")
+    container.id = "menu-container"
+    
+    menu.appendChild(container)
+
+    var title = document.createElement("span")
+    title.innerHTML = "Ausgewählte Zahl: " + (value === undefined ? "Bitte Zahl Auswählen" : value)
+    container.appendChild(title)
+
+
+    if (value != undefined) {
+
+        for (let i = 0; i < 10; i ++) {
+            var numberDiv = document.getElementById("header-number-" + i)
+            numberDiv.className = "number"
+            if (i == value) numberDiv.className += " active"
+        }
+
+        var rotateRight = document.createElement("button")
+        rotateRight.className = "turn-Right"
+        rotateRight.innerHTML = "Nach Rechts drehen"
+        rotateRight.addEventListener("click", function() {number[value].rotate(true)}, false)
+        
+        var rotateLeft = document.createElement("button")
+        rotateLeft.className = "turn-Left"
+        rotateLeft.innerHTML = "Nach Links drehen"
+        rotateLeft.addEventListener("click", function() {number[value].rotate(false)}, false)
+        
+        container.appendChild(rotateLeft)
+        container.appendChild(rotateRight)
+        
+        if (this.insertedX == -1) {
+            var insertNumber = document.createElement("button")
+            insertNumber.className = "insertNumber"
+            insertNumber.innerHTML = "Nummer im Grid einfügen"
+            insertNumber.addEventListener("click", function() {insertNumberToGrid()}, false)
+            container.appendChild(insertNumber)
+        }
+        
+        if (this.insertedX != -1) {
+            var removeNumber = document.createElement("button")
+            removeNumber.className = "removeNumber"
+            removeNumber.innerHTML = "Nummer aus Grid entfernen"
+            removeNumber.addEventListener("click", function() {removeNumberFromGrid()}, false)
+            container.appendChild(removeNumber)
         }
     }
 }
 
-function updateBoard() {
+function insertNumberToGrid() {
 
 }
 
+function removeNumberFromGrid() {
 
-// turn 90 deg
-// turn -90 deg
-// flip horizontally
+}
 
-// console.table(bord)
+this.showMenu()
 
-// console.table(numbers)
+
+var board = new Board()
